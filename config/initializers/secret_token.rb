b7d2f1ba815a330f2dbb83c9ac93476b4cedbf27
thomasvollath <tomv@kmtire.com>
2012-08-20 15:49:30 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnmExample1::Application.config.secret_token = '0cb412150d6b6fb159749ce6e3b5dc5b54111bfdad7caf6088dffad6a55c8a06974732e3d17bea5f6669f4f0b5405065bc2be8292e7eb948051f2be671866004'
